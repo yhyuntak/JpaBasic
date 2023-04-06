22381777264a5890b3e87760f4bf02da46968497
yhyuntak <yhyuntak@gmail.com>
2023-04-06 17:17:24 +0900

    기본 세팅

---
+++ src/main/java/com/example/hello_jpa/HelloJpaApplication.java
@@ -0,0 +1,34 @@
+package com.example.hello_jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class HelloJpaApplication {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		try {
+			Member member = new Member();
+			member.setName("HelloA");
+
+			em.persist(member);
+
+			tx.commit();
+		} catch (Exception e){
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
+	}
+
+}
---
+++ src/test/java/com/example/hello_jpa/HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
